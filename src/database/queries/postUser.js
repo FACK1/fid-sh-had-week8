f13dbad61fc50781d9c2b2c7fc67ece1b01e643d
hadeel_salamin <hadeel_salamin@hotmail.com>
2018-12-19 00:05:57 +0200

    rewritting post user queries with promises to make it more readable later when calling them #5

--- src/database/queries/postUser.js
+++ src/database/queries/postUser.js
@@ -1,31 +1,40 @@
 const db_connection = require('../db_connection.js');
 
-const checkUser = (email, cb) => {
-    dbConnection.query(`select email from users where email = $1`, [email], (error, result) => {
-        if (err) {
-          console.log(err);
-          return cb(err);
-        }
-        cb(null, result.rows);
-      }
-    );
-  };
+const checkUser = (name, email, password) => {
+
+	return new Promise((resolve, reject) => {
+		db_connection.query(`select email from users where email = $1`, [email], (error, result) => {
+			try {
+				resolve({
+					params: [name, email, password],
+					userExist: result.rows
+				})
+			} catch (e) {
+				reject(error);
+			}
+		});
+	})
+}
 
 
-const addUser = (name, email, password, cb) => {
+const addUser = obj => {
 
-  const q = 'INSERT INTO users (name, email,password) values ($1, $2, $3)'
+	return new Promise((resolve, reject) => {
 
-  db_connection.query(q, [name, email, password], (error, result) => {
-    if(error){
-      console.log(error);
-      return cb(error);
-    }
-    cb(null)
-  })
+		if (obj.userExist.length <= 0) {
+			db_connection.query('INSERT INTO users (name, email,password) values ($1, $2, $3)', obj.params, (error, result) => {
+				if (error) {
+					reject(error)
+				}
+				resolve('added')
+			})
+		} else {
+			resolve('already exist')
+		}
+	})
 }
 
 module.exports = {
   checkUser,
   addUser
-};
+}
