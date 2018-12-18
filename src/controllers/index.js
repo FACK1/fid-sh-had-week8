9538bd83a7a28709addbb4d28a251542f424a70c
shaima96 <141062@ppu.edu.ps>
2018-12-18 15:51:01 -0600

    writting errors handlers and image for errors #4

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const search = require('./search');
 const home = require('./home');
+const error=require('./error');
 const router = express.Router();
 
 
 router.get('/', home.get);
 router.post('/search', search.post);
+router.use(error.pageNotFound);
+router.use(error.serverError);
 
 
 module.exports = router;
