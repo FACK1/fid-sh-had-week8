aafe4b0df5eb395f2f37468ceb718618de42a1af
shaima96 <141062@ppu.edu.ps>
2018-12-19 19:48:26 -0600

    added validation for server side 'login' #1

--- src/validation/server-side.js
+++ src/validation/server-side.js
@@ -1,5 +1,10 @@
 const Joi = require('joi');
 
+const loginValidation = {
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+};
+
 const signupValidation = {
   name: Joi.string().required(),
   email: Joi.string().email().required(),
@@ -8,5 +13,6 @@ const signupValidation = {
 };
 
 module.exports = {
+  loginValidation,
   signupValidation
 }
